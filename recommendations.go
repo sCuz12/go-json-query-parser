3f00f84e4201d3f1de0140c8e1a3bdd7b5ad2e12
georgehadjisavva <georgex8@gmail.com>
2024-07-19 13:19:33 +0300

    change logic based on value type (add <,>)

--- recommendations.go
+++ recommendations.go
@@ -23,12 +23,31 @@ func (q *Query) GenerateRecommendations(jsonData string) ([]string, error) {
 	randomizeSample := sliceutils.RandomSubset(data, 6).([]map[string]interface{})
 
 	for _, item := range randomizeSample{
-		randomNumber := rand.Intn(2)
 		for key, value := range item {
-			if randomNumber == 1 {
-				recommendationsQuery = append(recommendationsQuery, fmt.Sprintf("select %v where %v=%v", key, key, value))
-			} else {
-				recommendationsQuery = append(recommendationsQuery, fmt.Sprintf("select * where %v=%v", key, value))
+			switch v:= value.(type) {
+				case float64 : {
+					randomNumber := rand.Intn(3) //0, 1 ,2
+
+					switch randomNumber {
+					case 0:
+						recommendationsQuery = append(recommendationsQuery, fmt.Sprintf("select %v where %v=%v", key, key, v))
+					case 1:
+						recommendationsQuery = append(recommendationsQuery, fmt.Sprintf("select %v where %v<%v", key, key, v))
+					case 2:
+						recommendationsQuery = append(recommendationsQuery, fmt.Sprintf("select %v where %v>%v", key, key, v))
+					}
+				} 
+				case string : {
+					randomNumber := rand.Intn(2) // 
+
+					if randomNumber == 1 {
+						recommendationsQuery = append(recommendationsQuery,fmt.Sprintf("select %v where %v=%v", key, key, value) )
+					} else { 
+						recommendationsQuery = append(recommendationsQuery, fmt.Sprintf("select * where %v=%v", key, value))
+					}
+				}
+				default : 
+				
 			}
 		}
 	}
